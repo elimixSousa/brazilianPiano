051227390bb13355f9fb0144156cebe0f0b339f8
Elisandro Sousa <157408762+elimixSousa@users.noreply.github.com>
2024-09-11 18:41:28 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,86 @@
+const song = document.getElementById('audio');
+const play = document.getElementById('play');
+
+// It starts true, 'cause when someone loads the page the song plays automatically.
+let isPlaying = true;
+
+function playSong() {
+    play.querySelector('.bi').classList.remove('bi-play-fill');
+    play.querySelector('.bi').classList.add('bi-pause-fill');
+    song.play();
+    isPlaying = true;
+};
+function pauseSong() {
+    play.querySelector('.bi').classList.remove('bi-pause-fill');
+    play.querySelector('.bi').classList.add('bi-play-fill');
+    song.pause();
+    isPlaying = false;
+};
+
+/*Automatically plays the song when someone loads the page,
+before the decider function.
+*/
+playSong();
+
+function playPauseDecider() {
+    if (isPlaying === true) {
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+}
+
+play.addEventListener('click', playPauseDecider);
+
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', function () {
+    pesquisar();
+    return false;
+});
+
+function pesquisar() {
+    let section = document.getElementById('search-results');
+
+    let searchField = document.getElementById('search-field').value;
+
+    if (!searchField) {
+        section.innerHTML = "<p class ='no-results'>Nenhum compositor ou música foi encontrado. <br> Você precisa digitar o nome de um compositor clássico brasileiro ou o nome de uma música.</p>";
+        return;
+    };
+
+    searchField = searchField.toLowerCase();
+
+    let results = "";
+
+    let name = "";
+    let about = "";
+    let link = "";
+    let video = "";
+    let tags = "";
+
+    for (let data of allData) {
+        name = data.name.toLowerCase();
+        about = data.about.toLowerCase();
+        video = data.video.toLowerCase();
+        tags = data.tags.toLowerCase();
+
+        if (name.includes(searchField) || about.includes(searchField) || tags.includes(searchField)) {
+            results += `
+                <div class="item-result">
+                    <h2>
+                        <a href= ${data.link} target="_blank">${data.name}</a>
+                    </h2>
+                        <p class="about-meta">${data.about}</p>
+                        <a class="video-link" href=${data.video} target="_blank">🔗Vídeo</a>
+                </div>
+            `;
+        };
+    };
+
+    if (!results) {
+        results = "<p class='no-results'>Nenhum compositor/música foi encontrado!</p>";
+    };
+
+    section.innerHTML = results;
+};
---
+++ js/database.js
@@ -0,0 +1,58 @@
+let allData = [
+    {
+        name: 'Antonio Carlos Gomes',
+        about: ' Foi o mais importante compositor de ópera brasileiro. Fiel ao estilo romântico, obteve carreira de destaque na Europa, sendo o primeiro brasileiro a ter suas obras apresentadas no Teatro Alla Scala de Milão. É o autor da ópera "O Guarani" (1870), cuja abertura é a vinheta do programa de rádio "A Voz do Brasil."',
+        link: 'https://musicabrasilis.org.br/compositores/antonio-carlos-gomes',
+        video: 'https://www.youtube.com/watch?v=lECFimiF-xo',
+        tags: 'antonio piano o guarani guarani gomes carlos'
+    },
+    {
+        name: 'Pe. José Maurício Nunes Garcia',
+        about: ' Um dos principais compositores do período colonial brasileiro, suas obras sacras são consideradas um marco na música erudita do país. Sua música, com influências portuguesas e italianas, é marcada pela polifonia e pelo contraponto.',
+        link: 'https://musicabrasilis.org.br/compositores/jose-mauricio-nunes-garcia',
+        video: 'https://www.youtube.com/watch?v=kSFPkaLiOo4',
+        tags: 'padre jose mauricio nunes garcia musica sacra polifonia contraponto piano'
+    },
+    {
+        name: 'Chiquinha Gonzaga',
+        about: ' Pioneira da música popular brasileira, Chiquinha Gonzaga também compôs obras eruditas. É conhecida por suas polcas e valsas, e por ter quebrado barreiras de gênero ao se dedicar à composição musical.',
+        link: 'https://musicabrasilis.org.br/compositores/chiquinha-gonzaga',
+        video: 'https://www.youtube.com/watch?v=aMCoI2VNz7Y',
+        tags: 'chiquinha gonzaga polca valsa pioneira musica popular piano'
+    },
+    {
+        name: 'Alberto Nepomuceno',
+        about: ' Um dos principais representantes do impressionismo musical no Brasil, Nepomuceno compôs obras sinfônicas, camerísticas e para piano, explorando a sonoridade da natureza e da cultura brasileira.',
+        link: 'https://musicabrasilis.org.br/compositores/alberto-nepomuceno',
+        video: 'https://www.youtube.com/watch?v=qdGq71OLnBk',
+        tags: 'alberto nepomuceno impressionismo sinfonia camara piano'
+    },
+    {
+        name: 'Heitor Villa-Lobos',
+        about: ' A figura mais emblemática da música clássica brasileira, Villa-Lobos compôs uma vasta obra que abrangeu desde a música erudita até a popular. Suas composições são marcadas pela originalidade e pela exploração da sonoridade dos instrumentos brasileiros.',
+        link: 'https://musicabrasilis.org.br/compositores/heitor-villa-lobos',
+        video: 'https://www.youtube.com/watch?v=bEAlSCqwBqQ',
+        tags: 'heitor villa-lobos villa lobos vila musica classica brasil instrumentos brasileiros piano bachianas piano'
+    },
+    {
+        name: 'Mozart Camargo Guarnieri',
+        about: ' Um dos principais nomes do nacionalismo musical brasileiro, Guarnieri compôs obras que exploram a cultura e a identidade brasileiras. Suas sinfonias e concertos são considerados marcos do nacionalismo musical.',
+        link: 'https://musicabrasilis.org.br/compositores/camargo-guarnieri',
+        video: 'https://www.youtube.com/watch?v=t3L_5Azzxvw',
+        tags: 'mozart camargo guarnieri nacionalismo sinfonia concerto danca ritmo piano'
+    },
+    {
+        name: 'Henrique Oswald',
+        about: ' Um dos primeiros compositores brasileiros a estudar na Europa, Oswald introduziu elementos da música europeia no Brasil. Suas óperas e peças para piano são exemplos de sua produção. Sua obra "Valsa Lenta", lembra muito a graciosidade e embalo das composições de Fredéric Chopin.',
+        link: 'https://musicabrasilis.org.br/compositores/henrique-oswald',
+        video: 'https://www.youtube.com/watch?v=tRpG3LRzLos',
+        tags: 'opera piano europa henrique oswald piano chopin'
+    },
+    {
+        name: 'Ernesto Nazareth',
+        about: ' Compositor de choro, Nazareth também compôs obras eruditas. Suas valsas e maxixes são considerados clássicos da música brasileira.',
+        link: 'https://musicabrasilis.org.br/compositores/ernesto-nazareth',
+        video: 'https://www.youtube.com/watch?v=2fhAFSb-4rU',
+        tags: 'ernesto nazareth choro valsa maxixe odeon chorinho piano'
+    }
+];
