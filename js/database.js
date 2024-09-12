03ce02741b3b21c03216beb2dc39cd030a42485f
Elisandro Sousa <157408762+elimixSousa@users.noreply.github.com>
2024-09-11 22:08:21 -0300

    Update database.js

--- js/database.js
+++ js/database.js
@@ -4,55 +4,62 @@ let allData = [
         about: ' Foi o mais importante compositor de ópera brasileiro. Fiel ao estilo romântico, obteve carreira de destaque na Europa, sendo o primeiro brasileiro a ter suas obras apresentadas no Teatro Alla Scala de Milão. É o autor da ópera "O Guarani" (1870), cuja abertura é a vinheta do programa de rádio "A Voz do Brasil."',
         link: 'https://musicabrasilis.org.br/compositores/antonio-carlos-gomes',
         video: 'https://www.youtube.com/watch?v=lECFimiF-xo',
-        tags: 'antonio piano o guarani guarani gomes carlos'
+        tags: 'antonio piano o guarani guarani gomes carlos musica música classica clássico classico clássica'
     },
     {
         name: 'Pe. José Maurício Nunes Garcia',
         about: ' Um dos principais compositores do período colonial brasileiro, suas obras sacras são consideradas um marco na música erudita do país. Sua música, com influências portuguesas e italianas, é marcada pela polifonia e pelo contraponto.',
         link: 'https://musicabrasilis.org.br/compositores/jose-mauricio-nunes-garcia',
         video: 'https://www.youtube.com/watch?v=kSFPkaLiOo4',
-        tags: 'padre jose mauricio nunes garcia musica sacra polifonia contraponto piano'
+        tags: 'padre jose mauricio nunes garcia musica sacra polifonia contraponto piano classico clássica classica música'
     },
     {
         name: 'Chiquinha Gonzaga',
         about: ' Pioneira da música popular brasileira, Chiquinha Gonzaga também compôs obras eruditas. É conhecida por suas polcas e valsas, e por ter quebrado barreiras de gênero ao se dedicar à composição musical.',
         link: 'https://musicabrasilis.org.br/compositores/chiquinha-gonzaga',
         video: 'https://www.youtube.com/watch?v=aMCoI2VNz7Y',
-        tags: 'chiquinha gonzaga polca valsa pioneira musica popular piano'
+        tags: 'chiquinha gonzaga polca valsa pioneira musica popular piano classico clássica classica lua branca música'
     },
     {
         name: 'Alberto Nepomuceno',
         about: ' Um dos principais representantes do impressionismo musical no Brasil, Nepomuceno compôs obras sinfônicas, camerísticas e para piano, explorando a sonoridade da natureza e da cultura brasileira.',
         link: 'https://musicabrasilis.org.br/compositores/alberto-nepomuceno',
         video: 'https://www.youtube.com/watch?v=qdGq71OLnBk',
-        tags: 'alberto nepomuceno impressionismo sinfonia camara piano'
+        tags: 'alberto nepomuceno impressionismo sinfonia camara piano classico clássica classica musica música'
     },
     {
         name: 'Heitor Villa-Lobos',
         about: ' A figura mais emblemática da música clássica brasileira, Villa-Lobos compôs uma vasta obra que abrangeu desde a música erudita até a popular. Suas composições são marcadas pela originalidade e pela exploração da sonoridade dos instrumentos brasileiros.',
         link: 'https://musicabrasilis.org.br/compositores/heitor-villa-lobos',
         video: 'https://www.youtube.com/watch?v=bEAlSCqwBqQ',
-        tags: 'heitor villa-lobos villa lobos vila musica classica brasil instrumentos brasileiros piano bachianas piano'
+        tags: 'heitor villa-lobos villa lobos vila musica classica brasil instrumentos brasileiros piano bachianas piano classico clássica'
     },
     {
         name: 'Mozart Camargo Guarnieri',
         about: ' Um dos principais nomes do nacionalismo musical brasileiro, Guarnieri compôs obras que exploram a cultura e a identidade brasileiras. Suas sinfonias e concertos são considerados marcos do nacionalismo musical.',
         link: 'https://musicabrasilis.org.br/compositores/camargo-guarnieri',
         video: 'https://www.youtube.com/watch?v=t3L_5Azzxvw',
-        tags: 'mozart camargo guarnieri nacionalismo sinfonia concerto danca ritmo piano'
+        tags: 'mozart camargo guarnieri nacionalismo sinfonia concerto danca ritmo piano classico clássica classica'
     },
     {
         name: 'Henrique Oswald',
         about: ' Um dos primeiros compositores brasileiros a estudar na Europa, Oswald introduziu elementos da música europeia no Brasil. Suas óperas e peças para piano são exemplos de sua produção. Sua obra "Valsa Lenta", lembra muito a graciosidade e embalo das composições de Fredéric Chopin.',
         link: 'https://musicabrasilis.org.br/compositores/henrique-oswald',
         video: 'https://www.youtube.com/watch?v=tRpG3LRzLos',
-        tags: 'opera piano europa henrique oswald piano chopin'
+        tags: 'opera piano europa henrique oswald piano chopin classico classica clássica'
     },
     {
         name: 'Ernesto Nazareth',
         about: ' Compositor de choro, Nazareth também compôs obras eruditas. Suas valsas e maxixes são considerados clássicos da música brasileira.',
         link: 'https://musicabrasilis.org.br/compositores/ernesto-nazareth',
         video: 'https://www.youtube.com/watch?v=2fhAFSb-4rU',
-        tags: 'ernesto nazareth choro valsa maxixe odeon chorinho piano'
+        tags: 'ernesto nazareth choro valsa maxixe odeon chorinho piano brasileiro chorinho classico clássica classica'
+    },
+    {
+        name: 'Zequinha de Abreu',
+        about: 'Músico, compositor e instrumentista brasileiro, famoso por seus choros. É autor do icônico "Tico-Tico no Fubá", um dos choros mais conhecidos mundialmente. Zequinha de Abreu também foi organizador e regente de orquestras e bandas.',
+        link: 'https://musicabrasilis.org.br/compositores/zequinha-de-abreu',
+        video: 'https://www.youtube.com/watch?v=xfKIrZpZQ5c',
+        tags: 'zequinha de abreu choro tico tico no fubá música brasileira piano brasileiro chorinho classico clássica classica'
     }
 ];
